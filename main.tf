4ba8eda571912c2333e409c2c856c5e376a62424
Mohammedirshaq <82198906+Mohammedirshaq@users.noreply.github.com>
2025-03-04 18:41:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "google" {
   project = "sam-452209"
-  region  = "us-central1"  # Add region
-  zone    = "us-central1-a" # Specify the zone
+  region  = "us-east1"
+  zone    = "us-east1-b"
+  credentials = file("file.json")
 }
 
 locals {
@@ -11,16 +12,21 @@ locals {
 resource "google_compute_instance" "inst1" {
   count        = length(local.l1)
   name         = local.l1[count.index]
-  machine_type = "e2-medium"
+  machine_type = "e2-micro"
+
+  labels = {
+    environment = "dev"
+    owner       = "team-xyz"
+  }
 
   boot_disk {
     initialize_params {
-      image = "projects/centos-cloud/global/images/family/centos-stream-9"
+      image = "centos-stream-9"
     }
   }
 
   network_interface {
     network = "default"
-    access_config {}  # This enables external IP
+    access_config {}
   }
 }
